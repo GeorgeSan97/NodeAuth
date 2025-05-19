6ded22e2f0610574b9deeeedee056daddeeb8be6
GeorgeSan97 <133137151+GeorgeSan97@users.noreply.github.com>
2025-05-19 17:20:34 -0500

    Creación de Servidor Express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hola mundo !!");
+import { Server } from './presentation/server';
+
+(() => {
+    main();
+})()
+
+
+async function main(){
+    //todo: await base de datos
+
+    //todo: inicio de nuestro servidor
+
+    new Server({
+        port: 3000
+    })
+    .start();
+}
